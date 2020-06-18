e2ee41a8ae3623625bffcedfa2c77b9a5bd98cd3
Ethan Alan Hereth <e.a.hereth@gmail.com>
2020-06-18 17:53:44 -0500

    Deal with backup source, destination directories
    This commit adds initial logic to handle backup source and destination
    directories. Error checking is done to make sure the source directory
    exist and the destination directory is successfully created. This is
    _not_ thoroughly tested.

--- backup.zsh
+++ backup.zsh
@@ -15,3 +15,19 @@ function create_backup_string () {
   #echo "$incr_prefix$backup_root$time_stamp"
 }
 
+backupSource="/Users/ehereth/Downloads"
+backupDest="/tmp/backups/"
+
+# Check for existence of backup directories.
+[[ ( -d $backupSource ) ]] && echo "backup source directory <$backupSource> exists" || print -P "%B%F{red}backup source directory <$backupSource> does not exist%f%b"
+[[ ( -d $backupDest ) ]] && echo "backup destination directory <$backupDest> exists" || print -P "%B%F{red}backup destination directory <$backupDest> does not exist%f%b"
+
+backupDir=$(create_backup_string)
+
+# Zsh colors are tricky.
+# I failed to get them to work with echo so I am using print -P.
+# https://wiki.archlinux.org/index.php/zsh#Colors
+# https://scriptingosx.com/2019/07/moving-to-zsh-06-customizing-the-zsh-prompt/
+print -nP "%B%F{green}Creating backup directory: <$backupDir>%f%b"
+
+mkdir $backupDest$backupDir && print -P "\t%B%F{green}-> Success!!!%f%b" || print -P "\n\t%B%F{red}Failed to create <$backupDir> ... exiting!!!%f%b"; exit
