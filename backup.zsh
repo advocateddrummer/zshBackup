dac850787168972320884a579c3339fd1ffde7c0
Ethan Alan Hereth <e.a.hereth@gmail.com>
2020-06-18 18:03:05 -0500

    Clean up backup destination variables

--- backup.zsh
+++ backup.zsh
@@ -1,6 +1,5 @@
 #!/usr/bin/env zsh
 
-
 function create_backup_string () {
 
   full_prefix="full_"
@@ -16,18 +15,19 @@ function create_backup_string () {
 }
 
 backupSource="/Users/ehereth/Downloads"
-backupDest="/tmp/backups/"
+backupDestRoot="/tmp/backups/"
 
 # Check for existence of backup directories.
 [[ ( -d $backupSource ) ]] && echo "backup source directory <$backupSource> exists" || print -P "%B%F{red}backup source directory <$backupSource> does not exist%f%b"
-[[ ( -d $backupDest ) ]] && echo "backup destination directory <$backupDest> exists" || print -P "%B%F{red}backup destination directory <$backupDest> does not exist%f%b"
+[[ ( -d $backupDestRoot ) ]] && echo "backup destination directory <$backupDestRoot> exists" || print -P "%B%F{red}backup destination directory <$backupDestRoot> does not exist%f%b"
 
 backupDir=$(create_backup_string)
+backupDest=$backupDestRoot$backupDir
 
 # Zsh colors are tricky.
 # I failed to get them to work with echo so I am using print -P.
 # https://wiki.archlinux.org/index.php/zsh#Colors
 # https://scriptingosx.com/2019/07/moving-to-zsh-06-customizing-the-zsh-prompt/
-print -nP "%B%F{green}Creating backup directory: <$backupDir>%f%b"
+print -nP "%B%F{green}Creating backup directory: <$backupDest>%f%b"
 
-mkdir $backupDest$backupDir && print -P "\t%B%F{green}-> Success!!!%f%b" || print -P "\n\t%B%F{red}Failed to create <$backupDir> ... exiting!!!%f%b"; exit
+mkdir $backupDest && print -P "\t%B%F{green}-> Success!!!%f%b" || print -P "\n\t%B%F{red}Failed to create <$backupDir> ... exiting!!!%f%b"; exit
