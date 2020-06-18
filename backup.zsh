94aea80456162e85d6b6aa919933a552591fbf81
Ethan Alan Hereth <e.a.hereth@gmail.com>
2020-06-18 18:07:36 -0500

    Tweak directory creation reporting
    This commit simply changes how the success or failure of backup
    destination directory creation is reported.

--- backup.zsh
+++ backup.zsh
@@ -28,6 +28,7 @@ backupDest=$backupDestRoot$backupDir
 # I failed to get them to work with echo so I am using print -P.
 # https://wiki.archlinux.org/index.php/zsh#Colors
 # https://scriptingosx.com/2019/07/moving-to-zsh-06-customizing-the-zsh-prompt/
-print -nP "%B%F{green}Creating backup directory: <$backupDest>%f%b"
+#print -nP "%B%F{green}Creating backup directory: <$backupDest>%f%b"
+print -P "Creating backup directory: <$backupDest>"
 
-mkdir $backupDest && print -P "\t%B%F{green}-> Success!!!%f%b" || print -P "\n\t%B%F{red}Failed to create <$backupDir> ... exiting!!!%f%b"; exit
+mkdir $backupDest && print -P "\t%B%F{green}Success!!!%f%b" || print -P "\t%B%F{red}Failed to create <$backupDir> ... exiting!!!%f%b"; exit
