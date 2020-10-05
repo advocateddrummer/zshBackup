5136221d0ca8193e2eb6296af755304bb278f223
Ethan Alan Hereth <e.a.hereth@gmail.com>
2020-10-05 11:59:37 -0500

    Add initial backup inventory fuctionality
    This commit adds logic to inventory old backups and remove backups that
    are no longer needed. This clearly need a lot of polish, but it is a
    start.

--- backup.zsh
+++ backup.zsh
@@ -39,6 +39,55 @@ backupDir=$(create_backup_string $backupType)
 [[ $? != 0 ]] && exit
 backupDest=$backupDestRoot$backupDir
 
+# Inventory old backups; the glob options (om) sort the results by modification
+# time, newest directories first while the (N) option causes this not to fail
+# if the array is empty.
+minutelyBackups=($backupDestRoot'minutely'*/(Nom))
+hourlyBackups=($backupDestRoot'hourly'*/(Nom))
+dailyBackups=($backupDestRoot'daily'*/(Nom))
+weeklyBackups=($backupDestRoot'weekly'*/(Nom))
+monthlyBackups=($backupDestRoot'monthly'*/(Nom))
+echo "There are $#minutelyBackups minutely backups"
+echo "There are $#hourlyBackups hourly backups:"
+echo "There are $#dailyBackups daily backups:"
+echo "There are $#weeklyBackups weekly backups:"
+echo "There are $#monthlyBackups monthly backups:"
+echo
+print -P "%B%F{green}${minutelyBackups[1,4]}%f%b %B%F{red}${minutelyBackups[5,-1]}%b%f"
+echo
+print -P "%B%F{green}${hourlyBackups[1,6]}%f%b %B%F{red}${hourlyBackups[7,-1]}%b%f"
+echo
+print -P "%B%F{green}${dailyBackups[1,7]}%f%b %B%F{red}${dailyBackups[8,-1]}%b%f"
+echo
+print -P "%B%F{green}${weeklyBackups[1,4]}%f%b %B%F{red}${weeklyBackups[5,-1]}%b%f"
+echo
+print -P "%B%F{green}${monthlyBackups[1,12]}%f%b %B%F{red}${monthlyBackups[13,-1]}%b%f"
+echo
+
+# Remove backups that are too old.
+for b (${minutelyBackups[5,-1]}) {
+  print -P "%B%F{red}Removing minutely backup $b%f%b"
+  rm -rf $b
+}
+for b (${hourlyBackups[7,-1]}) {
+  print -P "%B%F{red}Removing hourly backup $b%f%b"
+  rm -rf $b
+}
+for b (${dailyBackups[8,-1]}) {
+  print -P "%B%F{red}Removing daily backup $b%f%b"
+  rm -rf $b
+}
+for b (${weeklyBackups[5,-1]}) {
+  print -P "%B%F{red}Removing weekly backup $b%f%b"
+  rm -rf $b
+}
+for b (${monthlyBackups[12,-1]}) {
+  print -P "%B%F{red}Removing monthly backup $b%f%b"
+  rm -rf $b
+}
+
+echo
+
 # Zsh colors are tricky.
 # I failed to get them to work with echo so I am using print -P.
 # https://wiki.archlinux.org/index.php/zsh#Colors
